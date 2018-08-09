73365f9cde87f0e8583ca91ba3bf6119ed7bd04f
Ignacio Sanchez Gines <nacho@geardome.com>
2018-08-09 19:20:39 +0200

    Octal numbers syntax valid for python 3

--- example-molecule/ansible-role-banner/molecule/tests/test_default.py
+++ example-molecule/ansible-role-banner/molecule/tests/test_default.py
@@ -3,5 +3,5 @@ def test_files(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
-    assert f.mode == 0644
+    assert f.mode == 0o644
     assert f.md5sum == '58f62f1060075bab19139547f74e85fa'
