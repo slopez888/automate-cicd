0dead6a11752949371d84790e7a0daf9da9f76c7
Ramón Román Nissen <rromannissen@gmail.com>
2018-09-18 13:48:22 +0200

    Fixed lint errors in test

--- hackathons/rhte-2018-emea/tower-plugin-jenkins/playbooks/roles/install_wildfly_standalone/molecule/tests/test_default.py
+++ hackathons/rhte-2018-emea/tower-plugin-jenkins/playbooks/roles/install_wildfly_standalone/molecule/tests/test_default.py
@@ -10,15 +10,18 @@ def test_packages(host, package):
     pkg = host.package(package)
     assert pkg.is_installed
 
+
 def test_log_dir(host):
     dir = host.file('/var/log/wildfly/default')
     assert dir.exists
     assert dir.is_directory
 
+
 def test_service(host):
     srv = host.service('wildfly-default')
     assert srv.is_running
     assert srv.is_enabled
+
 
 def test_open_ports(host):
     s = host.socket("tcp://8080")
